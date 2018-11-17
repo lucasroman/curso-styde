9252e099bda6a1dc4cf6e6fc9310a2b7be07fc7a
Lucas Roman <lucasroman@gmail.com>
2018-11-17 18:26:09 -0300

    Add routes for users and their tests

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,27 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/users', function () {
+    return 'Users';
+});
+
+Route::get('/user/{id}', function($id) {
+    return "User's detail: {$id}";
+})->where('id','[0-9]+');
+
+Route::get('/user/new', function() {
+    return 'Create new user';
+});
+
+Route::get('/greeting/{name}/{nickname?}', function($name, $nickname = null) {
+    // Convert "nAmE" to "Name"
+    $name = ucfirst(strtolower($name));
+
+    if ($nickname) {
+        $nickname = ucfirst(strtolower($nickname));
+        return "Welcome user {$name} your nickname is '{$nickname}'.";
+    } else {
+        return "Welcome user {$name} you have not nickname.";
+    }
+});
---
+++ tests/Feature/UsersModuleTest.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Tests\Feature;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class UsersModuleTest extends TestCase
+{
+    /** @test */
+    public function it_load_users_list_page()
+    {
+        $this->get('/users')
+            ->assertStatus(200)
+            ->assertSee('Users'); // Work too for substring of "Users" e.g. "rs"
+    }
+
+    /** @test */
+    public function it_load_user_details_page()
+    {
+        $this->get('/user/5')
+            ->assertStatus(200)
+            ->assertSee("User's detail: 5"); // FIX!!! id MUST BE ONLY NUMBER!!!
+    }
+
+    /** @test */
+    public function it_load_new_user_page()
+    {
+        $this->get('/user/new')
+            ->assertStatus(200)
+            ->assertSee('Create new user');
+    }
+}
