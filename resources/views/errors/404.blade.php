1f54529c821100064725cf2dfe2a8c7d84a4a0a3
Lucas Roman <lucasroman@gmail.com>
2019-01-19 18:20:20 -0300

    Delete design on page 404

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -1,9 +1,6 @@
-@extends('layout')
-
-@section('title', "Page not found - ")
-
-@section('content')
-    <h1>Page not found</h1>
+<div style="text-align:center;">
+    <br>
+    <h1>404. Page not found</h1>
 
     <a href="{{ route('user.index') }}">Back to user list</a>
-@endsection
+</div>
