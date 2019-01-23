ce6811e702189bb806a0a1b84bfd5d3620f31bd5
Lucas Roman <lucasroman@gmail.com>
2019-01-23 11:56:10 -0300

    Change: refactoring user paths using 'resource'
    Using resource is a shorter way instead of making a route for each 
    action.

--- routes/web.php
+++ routes/web.php
@@ -14,21 +14,32 @@
 Route::get('/', function () {
     return view('welcome');
 });
-// Index --------------------------------------------
-Route::get('/users', 'UserController@index')
-    ->name('user.index');
 
-// Create -------------------------------------------
-Route::get('/users/create', 'UserController@create')
-->name('user.create');
+// // Index --------------------------------------------
+// Route::get('/users', 'UserController@index')
+//     ->name('users.index');
+//
+// // Create -------------------------------------------
+// Route::get('/users/create', 'UserController@create')
+// ->name('users.create');
+//
+// // Store --------------------------------------------
+// Route::post('/users', 'UserController@store')
+// ->name('users.store');
+//
+// // Show ---------------------------------------------
+// // This route work only request with the format "user/aNumber"
+// Route::get('/users/{user}', 'UserController@show')
+//     ->where('user','[0-9]+') // Avoid enter to other route as "user/new"
+//     ->name('users.show');
+// --------------------------------------------------
 
-Route::post('/users', 'UserController@store');
+// Short way to do the same thing above
+// Route::resource('users', 'UserController')
+//     ->only(['index', 'create', 'store', 'show']);
 
-// Show ---------------------------------------------
-// This route work only request with the format "user/aNumber"
-Route::get('/users/{user}', 'UserController@show')
-    ->where('user','[0-9]+') // Avoid enter to other route as "user/new"
-    ->name('user.show');
+Route::resource('users', 'UserController')
+    ->except(['edit', 'update', 'destroy']);
 
 // --------------------------------------------------
 Route::get('/greeting/{name}/{nickname?}', 'WelcomeUserController')
