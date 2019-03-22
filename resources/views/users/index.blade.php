06f7770b8fdeb4c468b9a1778b006864b565fd8b
Lucas Roman <lucasroman@gmail.com>
2019-03-22 10:22:22 -0300

    Add detele button to users index

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -10,10 +10,22 @@
                 <a href="{{ route('users.show', $user) }}">
                     <b>{{ $user->name }}</b>
                 </a>
+
                 {{ $user->email }}
+
                 <a href="{{ route('users.edit', $user) }}">
                     <b>{{ __('messages.user_edit') }}</b>
                 </a>
+
+                <form action="{{ route('users.destroy', $user) }}"
+                    method="POST">
+                    @csrf
+                    @method('DELETE')
+                    <button type="submit">
+                        <b>Delete</b>
+                    </button>
+
+                </form>
             </li>
         @empty
             <p>There are not users.</p>
